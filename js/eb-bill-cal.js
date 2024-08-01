f935f4fd531c4d1a9ef20e5cc7e66058df93276e
S-JAYASRI <jsridinesh@gmail.com>
2024-08-02 00:07:54 +0530

    file adding

--- js/eb-bill-cal.js
+++ js/eb-bill-cal.js
@@ -7,15 +7,14 @@ function convertUnits() {
     if(input<=50) {
         rate = input * 0.75;
     } 
-    else if(input>50 && input<=100) {
+    else if(input>50 && input<=150) {
         rate = (50 * 0.75) + (input - 50) * 1;
     }
-    else if (input>100 && input<=150) {
+    else if (input>150 && input<=200) {
         rate = (50 * 0.75) + (100 * 1) + (input - 150) * 1.30;
     }
-    else if (input>150 && input<=200) {
+    else {
         rate = (50 * 0.75) + (100 * 1) + (150 * 1.30) + (input - 200) * 1.50;
     }
     result.innerHTML = `The unit is ${input} and your EB bill amount is RS. ${rate} `;
 }
-
