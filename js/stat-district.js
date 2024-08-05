210138ff70c6dfa0ae392b1e9cdbb443ef37cf82
S-JAYASRI <jsridinesh@gmail.com>
2024-08-05 10:50:43 +0530

    file creating

--- js/stat-district.js
+++ js/stat-district.js
@@ -1,34 +1,41 @@
 
-addEventListener("click", displayStat)
+let button = document.querySelector("button");
+button.addEventListener("click", displayStat);
+let div = document.createElement("div");
+div.id = "result";
+document.querySelector(".main-box").appendChild(div);
+
 
 function displayStat() {
-    let dropDown = document.getElementById("select");
-    let city = dropDown.options[dropDown.SelectedIndex].value;
-    console.log(city);
+    const select = document.getElementById("select");
+    const city = select.options[select.selectedIndex].value;
+    // console.log(city);
     let population = 0, language = "", literacyRate = 0;
-    let text = `The Indian City of ${city} has a population of ${population}. Language spoken is ${language} and literacy rate is ${literacyRate}.`
-
     switch (city) {
-        case "Bangalore": 
-            population: 140080000;
-            language: "Kannada";
-            literacyrate: 88.69;
-            break;
-        case "Chennai":
-            population: 12053697;
-            language: "Tamil"
-            literacyrate: 90.23;
-            break;
-        case "Andra":
-            population: 26690000;
-            language: "Teleungu"
-            literacyrate: 67.02;
-            break;
-        case "Kerala":
-            population: 359567000;
-            language: "Malaiyalam"
-            literacyrate: 94.00;
-            break;
+        case 'Bangalore': 
+            population = 140080000
+            language = "Kannada"
+            literacyRate = 88.69
+            break
+        case 'Chennai':
+            population = 12053697
+            language = "Tamil"
+            literacyRate = 90.23
+            break
+        case 'Andra':
+            population = 26690000
+            language = "Teleungu"
+            literacyRate = 67.02
+            break
+        case 'Kerala':
+            population = 359567000
+            language = "Malayalam"
+            literacyRate = 94.00
+            break
     }
+
+    let text = `The Indian City of ${city} has a population of ${population}. Language spoken is ${language} and literacy rate is ${literacyRate}.`
+    document.getElementById("result").innerHTML = text;
+    
 }
 
